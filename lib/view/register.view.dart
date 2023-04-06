a617c06b2dc6a1caf9fb91b9731898186e95e96b
fatimazahrash <salehfatimzahra@gmail.com>
2023-04-06 12:06:34 +0200

    create register

---
+++ lib/view/register.view.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:register/view/button.global.dart';
+import 'package:register/view/social.login.dart';
+import 'package:register/view/text.form.global.dart';
+
+import '../utils/global.colors.dart';
+
+class RegisterView extends StatelessWidget {
+  RegisterView({Key? key}) : super(key: key);
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController fullnameController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController passwordconfController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*   const SizedBox(height: 20),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'registerhna',
+                    style: TextStyle(
+                      color: GlobalColors.mainColor,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ), // TextStyle
+                  ),
+                ),*/ // Text
+                const SizedBox(height: 30),
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/images/logo1.png',
+                    width: 230,
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+                Text(
+                  'sign up',
+                  style: TextStyle(
+                    color: GlobalColors.textColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ), // TextStyle
+                ),
+
+                const SizedBox(height: 15),
+
+                //////fullname Input
+                TextFormGlobal(
+                  controller: fullnameController,
+                  text: 'fullname',
+                  textInputType: TextInputType.text,
+                  obscure: false,
+                ),
+
+                /////email input
+                TextFormGlobal(
+                  controller: emailController,
+                  text: 'email',
+                  obscure: false,
+                  textInputType: TextInputType.emailAddress,
+                ),
+                /////password Input
+                TextFormGlobal(
+                  controller: passwordController,
+                  text: 'passwordoo',
+                  textInputType: TextInputType.text,
+                  obscure: true,
+                ),
+                ///// password conf input
+                TextFormGlobal(
+                  controller: passwordconfController,
+                  text: 'password conf',
+                  textInputType: TextInputType.text,
+                  obscure: true,
+                ),
+
+                const SizedBox(height: 10),
+                const ButtonGlobal(),
+                const SizedBox(height: 25),
+                const SocialRegister(),
+              ],
+            ), // Column
+          ), // Container
+        ), // SafeArea
+      ), // SingleChildScrollView
+    ); // Scaffold
+  }
+}
